7e06d704969282c5bd7fd1abb7588ed7c30bdca9
“Vasily <omzg@mail.ru>
2024-02-20 18:59:33 +0600

    functions added

---
+++ js/functions.js
@@ -0,0 +1,23 @@
+//Функция для проверки длины строки
+function getStirngLength(myString, maxLength) {
+  //если строка меньше или равна указанной длине
+  if(myString.length <= maxLength){
+    return true;
+  }else{
+    return false;
+  }
+}
+getStirngLength('десять сим', 10);
+
+//Функция для проверки, является ли строка палиндромом
+function isPalidrome(myString){
+  const temp = myString.replaceAll(' ', '');
+  const newString = temp.toLowerCase();
+  let emptyString = '';
+  for(let i = newString.length - 1; i >= 0; i--){
+    emptyString += newString[i];
+  }
+  return emptyString === newString;
+}
+
+isPalidrome('БУбуб');
